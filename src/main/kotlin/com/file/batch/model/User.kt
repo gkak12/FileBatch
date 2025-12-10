8e11911f7ac1bad823520cfca8e94ad50dca48e1
gkak12 <hyunjun5681@kakao.com>
2025-12-10 18:53:01 +0900

    사용자 클래스 테이블 매핑 및 ULID 적용
    (cherry picked from commit 0d361969ba0b41017692fba309175da0b7d16038)

--- src/main/kotlin/com/file/batch/model/User.kt
+++ src/main/kotlin/com/file/batch/model/User.kt
@@ -1,9 +1,35 @@
 package com.file.batch.model
 
+import com.github.f4b6a3.ulid.UlidCreator
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.PrePersist
+import jakarta.persistence.Table
+
+@Entity
+@Table(schema = "batch", name = "user_info")
 data class User(
+    @Id
+    @Column(length = 26)
+    var id: String? = null,
+
+    @Column(name = "first_name")
     var firstName: String? = null,
+
+    @Column(name = "last_name")
     var lastName: String? = null,
+
+    @Column(name = "email")
     var email: String? = null
 ) {
-    constructor() : this(null, null, null)
+    constructor() : this(null, null, null, null)
+
+    @PrePersist
+    fun generateUlid() {
+        if (id == null) {
+            // ULID 라이브러리를 사용
+            this.id = UlidCreator.getUlid().toString()
+        }
+    }
 }
