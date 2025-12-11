ef4718c8dc374f2f9ad07707616829f42acdd4bb
gkak12 <hyunjun5681@kakao.com>
2025-12-11 22:12:48 +0900

    Jasypt 빈 등록 및 테스트 코드 추가
    (cherry picked from commit fbbdc453661ad5e01a8964407899e1f424ffe431)

---
+++ src/test/kotlin/com/file/batch/config/JasyptConfigTest.kt
@@ -0,0 +1,51 @@
+package com.file.batch.config
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.slf4j.LoggerFactory
+
+class JasyptConfigTest {
+
+    private val log = LoggerFactory.getLogger(JasyptConfigTest::class.java)
+
+    private fun jasyptEncoding(value: String): String {
+        val pbeEnc = StandardPBEStringEncryptor()
+        pbeEnc.setAlgorithm("PBEWithMD5AndDES")
+        pbeEnc.setPassword(System.getenv("JASYPT_KEY"))
+
+        return pbeEnc.encrypt(value)
+    }
+
+    private fun jasyptDecoding(value: String): String {
+        val pbeEnc = StandardPBEStringEncryptor()
+        pbeEnc.setAlgorithm("PBEWithMD5AndDES")
+        pbeEnc.setPassword(System.getenv("JASYPT_KEY"))
+
+        return pbeEnc.decrypt(
+            value
+                .replace("ENC(", "")
+                .replace(")", "")
+        )
+    }
+
+    @Test
+    @DisplayName("jasypt 인코딩 테스트")
+    fun jasyptEncodingTest(){
+        val username = "root"
+        val password = "guswns124%"
+
+        log.info("username: {}", jasyptEncoding(username))
+        log.info("password: {}", jasyptEncoding(password))
+    }
+
+    @Test
+    @DisplayName("jasypt 디코딩 테스트")
+    fun jasyptDecodingTest(){
+        val username = "ENC(CP6FzQYT/Sv52TQi4/9U2Q==)"
+        val password = "ENC(36jn6vIZOaywDFHJOUEHGBwjf0ipBYv/)"
+
+        log.info("username: {}", jasyptDecoding(username))
+        log.info("password: {}", jasyptDecoding(password))
+    }
+}
